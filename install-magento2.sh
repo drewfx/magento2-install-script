62b69203c97cdb5c69fc46a2bf26af7c5ca6c605
Drew Ruppel <drewruppel@gmail.com>
2019-05-31 09:05:35 -0700

    refactor

--- install-magento2.sh
+++ install-magento2.sh
@@ -1,49 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
-# Gracefull Exit
+# Functions
+printMessage() {
+    echo "Permissions set for: $1"
+}
+
+# Graceful Exit
 set -e
 
 # Local Vars
-dir=$PWD
-approot=magento2
+root=magento2
+groupWrite=("var" "vendor" "generated" "pub/static" "pub/media" "app/etc")
 
-echo 'Starting Magento 2 Installation'
+echo "Starting Magento 2 Installation"
 
-# Download Projectso
-sudo composer create-project --repository=https://repo.magento.com/ magento/project-community-edition $approot
+# Download Project
+sudo composer create-project --repository=https://repo.magento.com/ magento/project-community-edition ${root}
 wait
 
 # Permissions To Deploy
-echo 'Settings Permissions'
-sudo chown -R www-data:www-data $approot
+printMessage ${root}
+sudo chown -R www-data:www-data ${root}
 
-echo "Changing Directory: $approot"
-cd "$approot"
+echo "Changing Directory: $root"
+cd "${root}"
 
-echo "In $PWD"
-
-echo 'Settings Permissions for Deployment Folders'
-sudo chmod -R g+w {var,vendor,generated,pub/static,pub/media,app/etc}
+for dir in "${groupWrite[@]}"; do
+  sudo chmod -R g+w ${dir}
+  printMessage ${dir}
+done
 sudo chmod u+x bin/magento
+echo "Permissions set for: bin/magento"
 wait
 
 # Configure Application & Install
-echo 'Install Magento2'
+echo "Install Magento2"
 php bin/magento setup:install --db-user=app --db-password=secret --base-url=http://m2local.com --backend-frontname=admin --admin-firstname=Drew --admin-lastname=Ruppel --admin-email=druppel@goldencomm.com --admin-user=druppel@goldencomm.com --admin-password=secret11 --language=en_US --currency=USD --use-rewrites=1 --use-sample-data
 wait
 
 # Deploy DI Compilation
-echo 'Compile DI Files'
+echo "Compile DI Files"
 php bin/magento setup:di:compile
 wait
 
 # Deploy Frontend Assets
-echo 'Deploy Static Content'
+echo "Deploy Static Content"
 php bin/magento setup:static-content:deploy -f
 wait
 
 # Final Permissions
+printMessage ${root}
 sudo chown -R www-data:www-data ./
 wait
 
-echo 'Magento 2 Installation Complete'
+echo "Magento 2 Installation Complete"
